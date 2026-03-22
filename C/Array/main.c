c5602af445b55e85dd24c89705cd81a26dccdf50
PDX PRO <pdxpro0098@gmail.com>
2026-03-22 12:00:00 +0530

    add inti and print array

--- C/Array/main.c
+++ C/Array/main.c
@@ -1,13 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct {
+typedef struct
+{
 	int *array;
 	int capacity;
 	int length;
-}DArray;
+} ARRAY;
 
-int main(){
-	
+void initARRAY(ARRAY *array)
+{
+	array->array = (int *)malloc(sizeof(int) * 2);
+	array->capacity = 2;
+	array->length = 0;
+}
+
+void printArray(ARRAY *array)
+{
+	for (int i = 0; i < array->length; i++)
+	{
+		printf("%d ", array->array[i]);
+	}
+}
+
+int main()
+{
+	ARRAY *arr;
+	initARRAY(arr);
+	push_back(arr, 900);
+	push_back(arr, 10);
+	printArray(arr);
+
 	return 0;
 }
