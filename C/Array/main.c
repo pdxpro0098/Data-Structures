30cf21c3e8b3dac240839db38196e329d20a33c7
PDX PRO <pdxpro0098@gmail.com>
2026-03-23 11:00:00 +0530

    add push back, front

--- C/Array/main.c
+++ C/Array/main.c
@@ -23,12 +23,50 @@ void printArray(ARRAY *array)
 	}
 }
 
+void push_back(ARRAY *array, int value)
+{
+	if (array->capacity == array->length)
+	{
+		array->array = realloc(array->array, sizeof(int) * (array->capacity + 2));
+		array->capacity += 2;
+		array->array[array->length++] = value;
+	}
+	else
+	{
+		array->array[array->length++] = value;
+	}
+}
+
+void push_front(ARRAY *array, int value)
+{
+	if (array->capacity == array->length)
+	{
+		array->array = realloc(array->array, sizeof(int) * (array->capacity + 2));
+		array->capacity += 2;
+	}
+
+	for (int i = array->length; i > 0; i--)
+	{
+		array->array[i] = array->array[i - 1];
+	}
+
+	array->array[0] = value;
+	array->length++;
+}
+
 int main()
 {
-	ARRAY *arr;
+	ARRAY *arr = (ARRAY *)malloc(sizeof(ARRAY));
 	initARRAY(arr);
-	push_back(arr, 900);
-	push_back(arr, 10);
+	push_front(arr, 1);
+	push_front(arr, 2);
+	push_front(arr, 3);
+	push_front(arr, 4);
+	push_front(arr, 5);
+	push_front(arr, 6);
+	push_front(arr, 7);
+	push_front(arr, 8);
+
 	printArray(arr);
 
 	return 0;
