73883d40db99d8f4f18e12ed67c22a0ad9bedefa
PDX PRO <pdxpro0098@gmail.com>
2026-04-03 19:10:15 +0530

    add push pop top function

--- C/Stack/Array_Stack/STACK.c
+++ C/Stack/Array_Stack/STACK.c
@@ -15,12 +15,41 @@ int Stack_Size(STACK *stack)
 
 void push(STACK *stack, int value)
 {
+    if (!stack)
+        return;
+
+    if (stack->size == stack->capacity)
+    {
+        stack->array = (int *)realloc(stack->array, sizeof(int) * stack->capacity + 2);
+        stack->capacity += 2;
+    }
+
+    stack->size++;
+    stack->top++;
+
+    stack->array[stack->top] = value;
 }
 
 void pop(STACK *stack)
 {
+
+    if (stack->top > -1)
+    {
+        stack->top--;
+        stack->size--;
+    }
+    else
+    {
+        printf("Stack is empty");
+    }
 }
 
 int top(STACK *stack)
 {
+    if (stack->top == -1)
+    {
+        return -1;
+    }
+
+    return stack->array[stack->top];
 }
