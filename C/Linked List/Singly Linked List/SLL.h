c7978ab154bee01e96f9149152767f199e5844ea
PDX PRO <pdxpro0098@gmail.com>
2026-04-09 14:36:09 +0530

    add list structure and common functions

---
+++ C/Linked List/Singly Linked List/SLL.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+
+typedef struct List
+{
+    int size;
+    Node *listHead;
+} List;
+
+void List_init(List *list);
+void insertHead(List *list, int value);
+void deleteHead(List *list);
+void insertTail(List *list, int value);
+void deleteTail(List *list);
---
+++ C/Linked List/Singly Linked List/main.c
@@ -0,0 +1,8 @@
+#include "SLL.h"
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
