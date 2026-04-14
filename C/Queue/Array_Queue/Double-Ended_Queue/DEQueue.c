c27e40bfa6b33bfc807351fbeacf46d595fb70b9
PDX PRO <pdxpro0098@gmail.com>
2026-04-14 15:42:30 +0530

    add enqueue front, rear

--- C/Queue/Array_Queue/Double-Ended_Queue/DEQueue.c
+++ C/Queue/Array_Queue/Double-Ended_Queue/DEQueue.c
@@ -26,10 +26,43 @@ int isEmpty(DEQueue *queue)
 
 void enqueueFront(DEQueue *queue, int value)
 {
-}
+    if (isFull(queue) || queue->front == 0)
+    {
+        printf("Cannot insert at front\n");
+        return;
+    }
 
+    if (queue->front == -1)
+    {
+        queue->front = queue->rear = 0;
+    }
+    else
+    {
+        queue->front--;
+    }
+
+    queue->array[queue->front] = value;
+    queue->size++;
+}
 void enqueueRear(DEQueue *queue, int value)
 {
+    if (isFull(queue) || queue->rear == queue->capacity - 1)
+    {
+        printf("Cannot insert at rear\n");
+        return;
+    }
+
+    if (queue->front == -1)
+    {
+        queue->front = queue->rear = 0;
+    }
+    else
+    {
+        queue->rear++;
+    }
+
+    queue->array[queue->rear] = value;
+    queue->size++;
 }
 
 int dequeueFront(DEQueue *queue)
