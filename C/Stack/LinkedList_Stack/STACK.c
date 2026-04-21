2307dab76d934f981d80ab5a74ab227eda2cfdf6
PDX PRO <pdxpro0098@gmail.com>
2026-04-21 12:40:45 +0530

    add init, top, size

--- C/Stack/LinkedList_Stack/STACK.c
+++ C/Stack/LinkedList_Stack/STACK.c
@@ -1,1 +1,31 @@
 #include "STACK.h"
+
+void STACK_init(STACK *stack)
+{
+    stack->size = 0;
+    stack->top = NULL;
+}
+int STACK_Size(STACK *stack)
+{
+    return stack->size;
+}
+
+Node *createNode(int value)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void push(STACK *stack, int value);
+int pop(STACK *stack);
+int top(STACK *stack)
+{
+    return stack->top;
+}
+
+int isEmpty(STACK *stack)
+{
+    return stack->size == 0;
+}
