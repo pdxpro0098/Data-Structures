ec286e9d64f0b27bdb4f6c25082f0ea736f67374
PDX PRO <pdxpro0098@gmail.com>
2026-04-21 14:40:45 +0530

    add push pop

--- C/Stack/LinkedList_Stack/STACK.c
+++ C/Stack/LinkedList_Stack/STACK.c
@@ -5,6 +5,7 @@ void STACK_init(STACK *stack)
     stack->size = 0;
     stack->top = NULL;
 }
+
 int STACK_Size(STACK *stack)
 {
     return stack->size;
@@ -18,11 +19,31 @@ Node *createNode(int value)
     return newNode;
 }
 
-void push(STACK *stack, int value);
-int pop(STACK *stack);
+void push(STACK *stack, int value)
+{
+    Node *newNode = createNode(value);
+    newNode->next = stack->top;
+    stack->top = newNode;
+    stack->size++;
+}
+
+int pop(STACK *stack)
+{
+    if (stack->top == NULL)
+    {
+        return -1;
+    }
+    Node *toDelete = stack->top;
+    int val = toDelete->data;
+    stack->top = toDelete->next;
+    free(toDelete);
+    stack->size--;
+    return val;
+}
+
 int top(STACK *stack)
 {
-    return stack->top;
+    return stack->top->data;
 }
 
 int isEmpty(STACK *stack)
