bab5a201efcc9d44b2b5afe02be0386aa785733a
PDX PRO <pdxpro0098@gmail.com>
2026-05-08 13:24:00 +0530

    add addchild
    it took so much time and reqired chatgpt help

--- C/Tree/General_Tree/TREE.c
+++ C/Tree/General_Tree/TREE.c
@@ -14,3 +14,30 @@ Node *createNode(int data)
 
     return newNode;
 }
+
+void addChild(Node *parent, int data)
+{
+    if (parent == NULL)
+        return;
+
+    Node *child = createNode(data);
+
+    if (child == NULL)
+        return;
+
+    if (parent->firstChild == NULL)
+    {
+        parent->firstChild = child;
+    }
+    else
+    {
+        Node *temp = parent->firstChild;
+
+        while (temp->nextSibling != NULL)
+        {
+            temp = temp->nextSibling;
+        }
+
+        temp->nextSibling = child;
+    }
+}
