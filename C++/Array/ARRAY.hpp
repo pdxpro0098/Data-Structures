3ad3392454140832520eac64d405e4856c7ea91e
PDX PRO <pdxpro0098@gmail.com>
2026-05-27 19:54:59 +0530

    docs: remove referance from vector

--- C++/Array/ARRAY.hpp
+++ C++/Array/ARRAY.hpp
@@ -24,32 +24,3 @@ public:
     int front();
     int back();
 };
-
-/*
-
-Capacity:
-size	Return size (public member function)
-max_size	Return maximum size (public member function)
-resize	Change size (public member function)
-capacity	Return size of allocated storage capacity (public member function)
-empty	Test whether vector is empty (public member function)
-reserve	Request a change in capacity (public member function)
-shrink_to_fit	Shrink to fit (public member function)
-
-Element access:
-operator[]	Access element (public member function)
-at	Access element (public member function)
-front	Access first element (public member function)
-back	Access last element (public member function)
-data	Access data (public member function)
-
-Modifiers:
-assign	Assign vector content (public member function)
-push_back	Add element at the end (public member function)
-pop_back	Delete last element (public member function)
-insert	Insert elements (public member function)
-erase	Erase elements (public member function)
-swap	Swap content (public member function)
-clear	Clear content (public member function)
-emplace	Construct and insert element (public member function)
-emplace_back	Construct and insert element at the end (public member function)*/
