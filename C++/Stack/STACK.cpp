73137dd4f4f53ebc3b6c3cee52e53d620da187e6
PDX PRO <pdxpro0098@gmail.com>
2026-05-29 12:50:00 +0530

    feat(stack.cpp): add resize, pop, push

--- C++/Stack/STACK.cpp
+++ C++/Stack/STACK.cpp
@@ -10,6 +10,15 @@ STACK::STACK()
 
 void STACK::resize()
 {
+    int *temp = array;
+    capacity += 2;
+    array = new int[capacity];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = temp[i];
+    }
+
+    delete[] temp;
 }
 
 int STACK::top()
@@ -22,12 +31,24 @@ int STACK::size()
     return length;
 }
 
-void STACK::push(int)
+void STACK::push(int value)
 {
+    if (length == capacity)
+    {
+        resize();
+    }
+
+    array[++Top] = value;
+    length++;
 }
 
 void STACK::pop()
 {
+    if (!empty())
+    {
+        Top--;
+        length--;
+    }
 }
 
 bool STACK::empty()
--- C++/Stack/main.cpp
+++ C++/Stack/main.cpp
@@ -1,7 +1,15 @@
 #include "STACK.hpp"
+#include <iostream>
 
 int main()
 {
-    
+    STACK s;
+    s.push(12);
+    s.push(13);
+    s.push(14);
+    s.push(15);
+    s.push(16);
+    s.pop();
+    std::cout << s.top();
     return 0;
 }
