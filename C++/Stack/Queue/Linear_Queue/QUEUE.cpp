3a46c0f834c817fc5a83112e158d0d4cf750d705
PDX PRO <pdxpro0098@gmail.com>
2026-06-17 14:58:00 +0530

    add: peek, size, isempty, constructor destructor

--- C++/Stack/Queue/Linear_Queue/QUEUE.cpp
+++ C++/Stack/Queue/Linear_Queue/QUEUE.cpp
@@ -1,2 +1,53 @@
 #include "QUEUE.hpp"
 
+QUEUE::QUEUE()
+{
+    front = -1;
+    rear = -1;
+    length = 0;
+    capacity = 10;
+    array = new int[capacity];
+}
+
+QUEUE::QUEUE(int size)
+{
+    front = -1;
+    rear = -1;
+    length = 0;
+    capacity = size;
+    array = new int[size];
+}
+
+int QUEUE::peek()
+{
+    if (isEmpty())
+    {
+        std::cout << "Queue is Empty";
+        return 0;
+    }
+
+    return array[front];
+}
+
+void QUEUE::enqueue(int value)
+{
+}
+
+void QUEUE::dequeue()
+{
+}
+
+int QUEUE::size()
+{
+    return length;
+}
+
+bool QUEUE::isEmpty()
+{
+    return length == 0;
+}
+
+QUEUE::~QUEUE()
+{
+    delete[] array;
+}
