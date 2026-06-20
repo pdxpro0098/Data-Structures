18439ee2d8b923e071b4d3b38d2f0ac4f47eec19
PDX PRO <pdxpro0098@gmail.com>
2026-06-20 12:16:00 +0530

    add structure for circular linked list

---
+++ C++/LinckedList/Circular_LinkedList/CLL.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class LLIST
+{
+private:
+    class Node
+    {
+    public:
+        Node *next;
+        int data;
+
+        Node();
+        static Node *create_node(int);
+    };
+    Node *head;
+    int length;
+
+public:
+    LLIST();
+    LLIST(int);
+    void push_front(int);
+    void push_back(int);
+    void pop_front();
+    void pop_back();
+    void insert_at(int, int);
+    void remove_at(int);
+
+    int front();
+    int back();
+    int size();
+    int is_empty();
+
+    friend void printList(LLIST list);
+};
