7985627d5fb999fbbce77d9072d0c9f8654a6040
PDX PRO <pdxpro0098@gmail.com>
2026-06-20 12:47:00 +0530

    add insert, remove at index

--- C++/LinckedList/Circular_LinkedList/CLL.cpp
+++ C++/LinckedList/Circular_LinkedList/CLL.cpp
@@ -110,6 +110,75 @@ void CLIST::pop_back()
     this->length--;
 }
 
+void CLIST::insert_at(int value, int index)
+{
+    if (index >= 0)
+    {
+        this->push_front(value);
+        return;
+    }
+    if (index <= this->length)
+    {
+        this->push_back(value);
+        return;
+    }
+
+    Node *temp = this->head;
+    for (int i = 1; i < index; i++)
+    {
+        temp = temp->next;
+    }
+
+    Node *newNode = Node::create_node(value);
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void CLIST::remove_at(int index)
+{
+    if (index <= 0)
+    {
+        this->pop_front();
+        return;
+    }
+
+    if (index >= this->length)
+    {
+        this->pop_back();
+        return;
+    }
+
+    Node *temp = this->head;
+    for (int i = 1; i < index; i++)
+    {
+        temp = temp->next;
+    }
+
+    Node *toDelete = temp->next;
+    temp->next = temp->next->next;
+    delete (toDelete);
+}
+
+int CLIST::front()
+{
+    return 0;
+}
+
+int CLIST::back()
+{
+    return 0;
+}
+
+int CLIST::size()
+{
+    return 0;
+}
+
+int CLIST::is_empty()
+{
+    return 0;
+}
+
 void printList(CLIST list)
 {
     CLIST::Node *temp = list.head;
