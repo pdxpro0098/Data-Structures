c124653ed485d4e093f2668c6f07c4c451aed281
PDX PRO <pdxpro0098@gmail.com>
2026-06-25 12:59:00 +0530

    add constructor for node, tree

--- C++/Tree/Binary_tree/TREE.cpp
+++ C++/Tree/Binary_tree/TREE.cpp
@@ -1,2 +1,28 @@
 #include "TREE.hpp"
 
+TREE::Node::Node()
+{
+    this->data = 0;
+    this->left = this->right = nullptr;
+};
+
+TREE::Node *TREE::Node::createNode(int data) {
+};
+
+TREE::Node::Node(int data)
+{
+    this->data = data;
+    this->left = this->right = nullptr;
+};
+
+TREE::TREE()
+{
+    this->size = this->height = 0;
+    this->root = nullptr;
+};
+
+TREE::TREE(int data)
+{
+    this->size = this->height = 1;
+    this->root = Node::createNode(data);
+};
