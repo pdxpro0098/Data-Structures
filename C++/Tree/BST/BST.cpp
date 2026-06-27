501da6b8ebca6f179fd62b88d1c56ca160e073fd
PDX PRO <pdxpro0098@gmail.com>
2026-06-27 14:30:00 +0530

    feat(C++/TREE/BST): add addChild, preOrder

--- C++/Tree/BST/BST.cpp
+++ C++/Tree/BST/BST.cpp
@@ -2,13 +2,30 @@
 
 BST::Node::Node()
 {
+    this->data = 0;
+    this->left = this->right = nullptr;
 }
 
-BST::Node::Node(int data)
+BST::Node::Node(int value)
 {
+    this->data = value;
+    this->left = this->right = nullptr;
 }
 
-BST::Node *BST::Node::createNode(int data)
+BST::Node *BST::Node::createNode(int value)
 {
+    Node *newNode = new Node(value);
+    return newNode;
+}
 
+BST::BST()
+{
+    this->_size = 0;
+    this->root = nullptr;
+}
+
+BST::BST(int value)
+{
+    this->_size = 1;
+    this->root = Node::createNode(value);
 }
